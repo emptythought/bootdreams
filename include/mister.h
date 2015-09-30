c77cb537eb1c1f619950219ed470293180881494
emptythought <cyle.terry@gmail.com>
2015-09-30 11:23:48 -0700

    Update mister.h
    Copyright year 2015

--- include/mister.h
+++ include/mister.h
@@ -1,7 +1,7 @@
 /*
     This file is part of BootDreams.
 
-    Copyright (C) 2010 Cyle Terry
+    Copyright (C) 2010, 2015 Cyle Terry
 
     BootDreams is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
