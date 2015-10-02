8debfd13996ebdf527dad73d7dbc24f0b93130c7
emptythought <cyle.terry@gmail.com>
2015-10-02 10:16:16 -0700

    Update readbf.h
    Merging old mister with readbf

--- include/readbf.h
+++ include/readbf.h
@@ -18,6 +18,59 @@
 */
 
 #include "mister.h"
+/*
+    This file is part of BootDreams.
+
+    Copyright (C) 2010, 2015 Cyle Terry
+
+    BootDreams is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BootDreams is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BootDreams.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define MR_MAX_COLORS 127
+
+#define MR_USER_MAX_SIZE 8192
+#define MR_USER_WIDTH 320
+#define MR_USER_HEIGHT 90
+
+#define MR_SEGA_SIZE 4311
+#define MR_SEGA_WIDTH 291
+#define MR_SEGA_HEIGHT 89
+
+#define MR_TRADEMARK_SIZE 299
+#define MR_TRADEMARK_WIDTH 22
+#define MR_TRADEMARK_HEIGHT 12
+
+typedef struct {
+    char mr[MR_USER_MAX_SIZE];
+    char id[2];
+    unsigned short size;
+    char reserve_o1[6];
+    unsigned short offset;
+    char reserve_o2[2];
+    unsigned short width;
+    char reserve_o3[2];
+    unsigned short height;
+    char reserve_o4[6];
+    unsigned short colors;
+    char reserve_o5[2];
+    char palette[MR_MAX_COLORS * 4];
+    char bitmap[MR_USER_MAX_SIZE - 1];
+    char bitmap_expanded[MR_USER_WIDTH * MR_USER_HEIGHT];
+} typemr;
+
+int read_mr(char *mrf);
+int display_mr();
 
 typedef enum {
     unscrambled,
